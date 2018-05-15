84765350a7ebd535c68b25b952cfaeb498f7e390
Jeremy Mulenex <jeremy@aboutjm.com>
2018-05-15 08:50:33 -0500

    fixed landing content. adjusted width of the names...

--- map-carousel/src/title.js
+++ map-carousel/src/title.js
@@ -5,8 +5,6 @@ export default class title extends Component {
     return (
       <div className="landing slideContent">
         <h1>Why is that map so usable?</h1>
-        <h2> Willie and Jeremy</h2>
-        <h2> https://github.com/wnordmann/Foss4gNA2018</h2>
         <div className="names">
           <span className="first">
             <h3>Willie</h3>
@@ -17,7 +15,7 @@ export default class title extends Component {
             <p>jmulenex@boundlessgeo.com</p>
           </span>
         </div>
-        <h4>Are you happy now?</h4>
+        <h4>https://github.com/wnordmann/Foss4gNA2018</h4>
       </div>
     );
   }
