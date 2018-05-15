92b55172c5bc522699bc2b2ccdbd9b7754c501c7
Jeremy Mulenex <jeremy@aboutjm.com>
2018-05-14 21:41:47 -0500

    fixed title screen added image that does work...

--- map-carousel/src/title.js
+++ map-carousel/src/title.js
@@ -5,8 +5,17 @@ export default class title extends Component {
     return (
       <div className="landing slideContent">
         <h1>Why is that map so usable?</h1>
-        <h2> Willie and Jeremy</h2>
-        <h2> Are you happy now?</h2>
+        <div className="names">
+          <span className="first">
+          <h3>Willie</h3>
+          <p>wnordman@boundlessgeo.com</p>
+          </span>
+          <span>
+          <h3>Jeremy</h3>
+          <p>jmulenex@boundlessgeo.com</p>
+          </span>
+        </div>
+        <h4>Are you happy now?</h4>
       </div>
     );
   }
